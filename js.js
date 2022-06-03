f9dfde1a0c66fb439e12dea008a0107917185598
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2022-06-03 22:57:29 +0700

    alot

---
+++ js.js
@@ -0,0 +1,34 @@
+
+document.getElementById("title").innerHTML = "Trần Việt Thắng";
+
+
+//img
+var img = document.getElementById('img');
+img.src = "https://scontent.fsgn13-1.fna.fbcdn.net/v/t1.6435-9/198252225_537688927387355_5181228056368091539_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=y7HfVqLC-r8AX__yohI&_nc_oc=AQm5ZaBpqkPj7S3k2AHRvafUUqsujdy9Uv9InqK2h_P3wxlLwKZaL-exPJROabQCZ3Lp4y87VELYsWPQ8n6gNbaS&_nc_ht=scontent.fsgn13-1.fna&oh=00_AT-404aUX4IQlAOiToKfUJHVm6H1-s5ALYAIAK7IUzZ3zQ&oe=62BE845C";
+// tên
+document.getElementById("Name").innerHTML = "Trần Việt Thắng";
+
+
+// link fb
+var fb = document.getElementById('facebook_link');
+fb.href = "https://www.facebook.com/LDThangws";
+
+// link ig
+var ig = document.getElementById('ig_link');
+ig.href = "https://www.tiktok.com/@mood.underground?fbclid=IwAR2lFOJ7YBcTo382NfV9XHbCIIcgOPts-3QyYRKDj2xbmjNnAKa3t33StuA";
+
+// link gmail
+var gmail = document.getElementById('gmail_link');
+gmail.href = "https://www.youtube.com/channel/UC59TbtT8ltl4M5EHGGvGnzQ";
+
+//link sdt
+var phone = document.getElementById('phone_link');
+phone.href = "tel: 0832165254   ";
+
+//link map
+var map = document.getElementById('map_link');
+map.href = "https://www.google.com/maps/place/Vinhomes+Grand+Park/@10.8372192,106.828426,17z/data=!3m1!4b1!4m5!3m4!1s0x3175211a1d77cb9b:0xd8ec8eb530841276!8m2!3d10.8372192!4d106.8306147";
+
+
+
+
